ec7dbaed5dd3279f80778e3b76cf5d401a3929fa
Nathan Stuart <nathan.stuart01@gmail.com>
2017-11-19 15:58:48 -0700

    added function to read a snow csv and turn data into two lists, one with years, other with inches fallen in years

--- snow_csv_reader.py
+++ snow_csv_reader.py
@@ -2,10 +2,27 @@ import csv
 
 raw_snow_file = 'altasnow70s.csv'
 
-with open(raw_snow_file, 'rb') as f:
-	area_snow = []
-	reader = csv.reader(f)
-	your_list = list(reader)
+def snow_csv_stats_to_array(snow_csv_file):
+	snow_years = []
+	snow_inches = []
 
-print(your_list)
+	with open(snow_csv_file, 'r') as csv_snow_data:
+	    csv_reader = csv.reader(csv_snow_data)
+	    for row in csv_reader:
+	        snow_years.extend(row[0:1]) 
+	        snow_inches.extend(row[1:2])
+	        
+	snow_years.pop(0)
+	snow_inches.pop(0)
+	snow_years_int = [ int(x) for x in snow_years ]
+	snow_inches_int = [ int(x) for x in snow_inches ]
+
+	return snow_years_int, snow_inches_int
+	
+
+snow_years, snow_inches = snow_csv_stats_to_array(raw_snow_file)
+
+print(snow_years)
+print(snow_inches)
+
 
