3ba9363e5cca741acf967adca89f3ecd394fc8d6
Nathan Stuart <nathan.stuart01@gmail.com>
2018-01-29 08:39:07 -0700

    Added methods in base/24hr crawler to accont for value of tr

--- utahskiareas/areas_crawler.py
+++ utahskiareas/areas_crawler.py
@@ -9,45 +9,25 @@ class AreasCrawler(ScraperLib):
 	def __init__(self, area_name):
 		super().__init__()
 		self.area_name = area_name
-		self.base_total = 0
-		self.twenty_four_hour_total = 0
 
-	# do this as a for loop and each time craete a new variable, could it be a new class instance variable?
-		
-	# for example in the area, make self.base_total_alta a variable to be used as a class property
 	def get_base_total(self, area_url, base_selector, base_selector_index):
 		area_to_scrape = requests.get(area_url)
 		souped_area = BeautifulSoup(area_to_scrape.content, 'html.parser')
 		base_values = souped_area.find_all(class_=base_selector)[base_selector_index].text.encode('utf-8')
-		return base_values.decode("ascii", "ignore")
-
-
-		#use if/else logic to determine what area to select
-	def get_24_hr_total(self, area_url, twenty_four_hour_selector):
+		base_total = base_values.decode("ascii", "ignore").replace('"', '')
+		self.base_total = int(base_total)
+	
+	def get_24_hr_total(self, area_url, twenty_four_hour_selector, twenty_four_hour_index):
 		area_to_scrape = requests.get(area_url)
 		souped_area = BeautifulSoup(area_to_scrape.content, 'html.parser')
-		base_values = souped_area.find_all(class_=twenty_four_hour_selector)[2].text.encode('utf-8')
-		twenty_four_hour_total = base_values.decode("ascii", "ignore").replace('"', '')
-		twenty_four_hour_total_to_int = int(twenty_four_hour_total)
-		self.twenty_four_hour_total = twenty_four_hour_total_to_int
+		twenty_four_values = souped_area.find_all(class_=twenty_four_hour_selector)[twenty_four_hour_index].text.encode('utf-8')
+		twenty_four_total = twenty_four_values.decode("ascii", "ignore").replace('"', '').lower()
+		if 'tr' in twenty_four_total:
+			self.twenty_four_hour_total = 0
+		else:
+			self.twenty_four_hour_total = int(twenty_four_total) 
 		return self.twenty_four_hour_total
 
-
-		 # incorporate this into one function above, using if
-		"""def park_city(self, area_url):
-		park_city_scraped = requests.get(area_url)
-		park_city_souped = BeautifulSoup(park_city_scraped.content, 'html.parser')
-		park_city_values = park_city_souped.find_all('script', type='text/javascript')[1]
-		park_city_string = park_city_values.contents[0]
-		park_city_snow_data_searcher = re.compile(r'Inches\S\S\S\d+')
-		park_city_snow_data = park_city_snow_data_searcher.findall(park_city_string) 
-		park_city_base_raw = park_city_snow_data[4]
-		park_city_24hr_raw = park_city_snow_data[1]
-		park_city_base = park_city_base_raw.replace('Inches":"', '')
-		park_city_24hr = park_city_24hr_raw.replace('Inches":"', '')
-		return park_city_base, park_city_24hr
-		"""
-		# api scraper(if that is such a thing?)getter?
 	def scrape_sundance(self, stats_api):
 		sundance_api = requests.get(stats_api)
 		sundance_data = sundance_api.json()
--- utahskiareas/gather_snow_stats.py
+++ utahskiareas/gather_snow_stats.py
@@ -1,56 +1,14 @@
 from areas_crawler import AreasCrawler 
 from scraper_lib import ScraperLib
 
-
-# want to be able to have a list of all my area names
-# I want to be able to send off a job each day that gathers their data
-# I want to be able to saved that gathered data to a db for that areaa
-# I want to use the classes from above to do this
-
-
-# loop through the areas
-# for each area, fire off the method to get that areas data
-# save that areas data to a db
-# will need the data from the scraper lib to know what to crawl
-# will need the methods to crawl each area from the areas_crawler
-# make a loop, and depednding on the name of that area in the loop, that is the method I call, and I pass in the area selectors to each method
-
-
-#def get_area_selectors(areas):
-#	for area in areas:
-		# I have access to my selectors
-		# I want to get the base selectors and send those to their appropriate function to gather the data for
-		# I need to write the methods for each areas base selector
-		# I will import the areas crawler class and send the correct selectors to the correct methods to scrape, making objects from the for loop to do that
-
-
-
-			# set area name as a variable to pass to the function to use to select data with, function selects base/24hr
-			# for a for i in loop here to loop through as many times as the length of the area seletor lib
-
-#def get_base_24hr_totals_park_city(park_city_base_selector, park_city_24hr_selector):
-	# use the variables I created in the get area names
-	# create a crawl functdion here with the selectors to gather the data
-	# set those data gathered to a variable that I can then use to save to the specific db for that area
-
-#def save_base_24hr_totals_park_city(park_city_base_total, park_city_24hr_total):
-	#take the totals I crawled from the above function, save them to a database
-	# the entry will correspond to the ski areas unique id
-	# the entry will up made as a new one each day
 alta = AreasCrawler('alta')
-#TypeError: can only concatenate list (not "str") to listprint(alta.area_selector_library[0]['url'])
-#print(alta.area_selector_library[0]['base_selector'])
-#print(alta.area_selector_library[0]['base_selector_index'])
-
-#print(alta.get_base_selectors())
-#print(alta.get_24_hr_total('https://www.alta.com/'))
-#print(alta.twenty_four_hour_total)
 print(alta.get_base_total('https://wwww.alta.com', 'value', 3))
+print(alta.get_24_hr_total('https://wwww.alta.com', 'value', 2))
 
+snowbird= AreasCrawler('snowbird')
+print(snowbird.get_base_total('https://www.snowbird.com/', "sb-condition_value", 4))
+print(snowbird.get_24_hr_total('https://www.snowbird.com/', "sb-condition_value", 2))
 
-#= AreasCrawler('snowbird')
-#print(snowbird.twenty_four_hour_total)
-#print(snowbird.get_24_hr_total('https://www.snowbird.com/', "sb-condition_value"))
 
 
 
