5c0c77e0b2952b6fb54c742402f16d0b6c0df3f5
Nathan Stuart <nathan.stuart01@gmail.com>
2018-02-28 08:29:15 -0700

    added while loop to get all base data and ski area names

--- utahskiareas/gather_snow_stats.py
+++ utahskiareas/gather_snow_stats.py
@@ -8,6 +8,8 @@ class GatherSnowStats():
 		self.stat_type = stat_type
 
 	def crawl_data(self, data_to_gather):
+		base_names = []	
+		base_data = []
 		data_to_gather = self.stat_type
 		if data_to_gather == 'base':
 			areas = ScraperLib('all areas')
@@ -16,11 +18,12 @@ class GatherSnowStats():
 			while i < 14:
 				ski_area = AreasCrawler(area_info[i]['name'], area_info[i]['url'])
 				ski_area_base = ski_area.get_base_total(ski_area.area_url, area_info[i]['base_selector'], area_info[i]['base_selector_index'])
-				print(ski_area_base, ski_area.area_name)
+				base_data.append(ski_area_base)
+				base_names.append(ski_area.area_name)
 				i = i + 1
 		else:
 			print('No data yet to gather besides base data')
-
+		return base_data, base_names
 
 
 base_data_gatherer = GatherSnowStats('base')
