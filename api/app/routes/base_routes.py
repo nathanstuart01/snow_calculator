befcb4683d970ee806ae26ffcad18ee6314bbcb7
Nathan Stuart <nathanstuart@Jessicas-MBP.domain>
2019-04-26 12:02:39 -0600

    update

--- api/app/routes/base_routes.py
+++ api/app/routes/base_routes.py
@@ -32,7 +32,7 @@ def get_base_totals():
 					'crawled_at': base_data.crawled_at.strftime("%Y-%m-%d")
 			}
 		base_totals_data.append(obj)
-		response = jsonify(twenty_four_totals_data)
+		response = jsonify(base_totals_data)
 		response.headers.add('Access-Control-Allow-Origin', '*')
 		response.status_code = 200
 		return response
